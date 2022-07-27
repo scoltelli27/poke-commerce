0ec365a56f066f9631c0a38768d65d62f1f3ddf2
scoltelli27 <92338065+scoltelli27@users.noreply.github.com>
2022-07-27 16:50:10 -0300

    Create product API

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,34 @@
 let PokeDB = require("../model/model");
 
 //Create and save new product
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  //Validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content cannot be empty" });
+    return;
+  }
+
+  //New product
+  const product = new PokeDB({
+    title: req.body.title,
+    price: req.body.price,
+  });
+
+  //Save user in DB
+  product
+    .save(product)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({
+          message:
+            err.message || "Some error ocurred while creating a new product",
+        });
+    });
+};
 
 //Find product
 exports.find = (req, res) => {};
