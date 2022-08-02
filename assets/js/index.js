e03d2988279f0ac20fe6a462f75dd2119109ac8d
scoltelli27 <92338065+scoltelli27@users.noreply.github.com>
2022-08-02 18:38:12 -0300

    Cart interface working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,43 @@
 $("#add_product").submit(function (event) {
   alert("Data was stored");
 });
+
+const cartBtn = document.querySelector(".cart-btn");
+const overlay = document.querySelector(".cart-overlay");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector(".close-cart");
+const bagBtn = document.querySelectorAll(".bag-btn");
+const burgerBtn = document.querySelector(".nav-icon");
+const burgerMenu = document.querySelector(".burger");
+
+//Show cart and overlay
+cartBtn.addEventListener("click", function () {
+  overlay.classList.toggle("hidden");
+  cart.classList.toggle("showCart");
+});
+
+//Hide cart and overlay
+closeCart.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+  cart.classList.remove("showCart");
+});
+
+overlay.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+  cart.classList.remove("showCart");
+  burgerMenu.classList.remove("showBurger");
+});
+
+// Add event listener to each "add to basket" button
+bagBtn.forEach((bagBtn) => {
+  bagBtn.addEventListener("click", () => {
+    overlay.classList.toggle("hidden");
+    cart.classList.toggle("showCart");
+  });
+});
+
+//Burger menu
+burgerBtn.addEventListener("click", function () {
+  overlay.classList.toggle("hidden");
+  burgerMenu.classList.add("showBurger");
+});
